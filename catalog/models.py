1046fa65670b4ebe546caab2ddc9db767535241a
Artur Khuziakhmetov <husiakhmetov@yandexlyceum.ru>
2024-03-10 18:14:38 +0300

    In the middle of part 9

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
 
 from django.urls import (
     reverse,
@@ -111,8 +112,21 @@ class BookInstance(models.Model):
         help_text="Book availability",
     )
 
+    borrower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
